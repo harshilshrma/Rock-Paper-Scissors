ee1e753929f7fb00cb3e6f4d93846fe47d8b5ddb
Harshil Sharma <harshilsharma.dev@gmail.com>
2024-01-13 23:23:15 +0530

    built game logic (still left)

--- script.js
+++ script.js
@@ -10,3 +10,50 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".imgs");
+
+const generateCompChoice = () => {
+    // One of rock, paper and scissors
+    const options = ["rock", "paper", "scissor"];
+    const idx = Math.floor(Math.random() * 3);
+    return options[idx];
+}
+
+const drawGame = () => {
+    console.log("DRAW");
+}
+
+const playGame = (userChoice) => {
+    console.log("user choice is = ", userChoice);
+    // To generate computer's choice
+    const compChoice = generateCompChoice();
+    console.log("comp choice is = ", compChoice);
+
+    if (userChoice === compChoice) {
+        // Draw Game
+        drawGame();
+    } else {
+        let userWin = true;
+        if (userChoice === "rock") {
+            // comp shd be scissor or paper
+            userWin = compChoice === "paper" ? false : true;
+        } else if (userChoice === "paper") {
+            // comp shd be scissor or rock
+            userWin = compChoice === "scissor" ? false : true;
+        } else {
+            // comp shd be rock or paper
+            userWin = compChoice === "rock" ? false : true;
+        }
+    }
+}
+
+choices.forEach((choice) => {
+    choice.addEventListener("click", () => {
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
+    });
+});
